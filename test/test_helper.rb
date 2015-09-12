cba6fcfdc590e82e4d96e42c59fae79310140baf
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-09-12 15:40:53 -0300

    test: load pry if available

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,10 @@ require 'autorespawn'
 require 'minitest/autorun'
 require 'minitest/spec'
 require 'flexmock/minitest'
+
+if ENV['TEST_ENABLE_PRY'] != '0'
+    begin
+        require 'pry'
+    rescue Exception
+    end
+end
